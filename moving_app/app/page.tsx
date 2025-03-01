9d5612f8e72ada0be1181136bd79aadbf3ad20f2
Anna Kester <akester@middlebury.edu>
2025-03-01 12:33:03 -0500

    second commit anna

--- moving_app/app/page.tsx
+++ moving_app/app/page.tsx
@@ -8,7 +8,8 @@ export default async function Home() {
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         {/* {hasEnvVars ? <SignUpUserSteps /> : <LandingPage />} */}
-        <ItemList />
+        <ItemList 
+          searchTerm={""}/>
       </main>
     </>
   );
--- moving_app/components/itemList/ItemList.js
+++ moving_app/components/itemList/ItemList.js
@@ -1,10 +1,11 @@
 import ItemCard from "../itemCard/ItemCard";
 import listings from "../../lib/item-listings.json";
 
-export default function ItemList(){
+export default function ItemList({searchTerm}){
     
     const items = listings.map((listing) => (
         <ItemCard
+            key={listing.id}
             listing={listing}
         />
     ));
