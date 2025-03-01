fcd6a8c2184452804349b69712de74e0101c5e72
noahrizika <noahrizika@gmail.com>
2025-03-01 13:59:50 -0500

    update items components to use seed data

--- moving_app/components/itemList/ItemList.tsx
+++ moving_app/components/itemList/ItemList.tsx
@@ -9,9 +9,19 @@ export default function ItemList({ searchTerm }: { searchTerm?: string }) {
     );
   }
 
-  const items = listingsFiltered.map((listing) => (
-    <ItemCard key={listing.id} listing={listing} />
-  ));
+  const users = data.users;
+  const items = listingsFiltered.map((listing) => {
+    const user = users.filter((user) => user.id == listing.sellerId)[0];
+
+    return (
+      <ItemCard
+        key={listing.id}
+        listing={listing}
+        users={data.users}
+        userInfo={{ email: user.email, name: user.name }}
+      />
+    );
+  });
 
   return <div className="gap-y-8">{items}</div>;
 }
