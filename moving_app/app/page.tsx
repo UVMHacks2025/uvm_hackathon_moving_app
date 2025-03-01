00a03a01b251b46801dd0f60876b77778a1ff671
noahrizika <noahrizika@gmail.com>
2025-03-01 10:09:38 -0500

    initial commit

---
+++ moving_app/app/page.tsx
@@ -0,0 +1,13 @@
+import { LandingPage } from "@/components/landingPage/landing-page";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+export default async function Home() {
+  return (
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        {/* {hasEnvVars ? <SignUpUserSteps /> : <LandingPage />} */}
+        <LandingPage />
+      </main>
+    </>
+  );
+}
