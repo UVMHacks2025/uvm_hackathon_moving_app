7b2a4c3e683ce92c03e7b6ce125d7ba8ffd0ffb1
Neil Dcruze <neildcruze@gmail.com>
2025-03-01 11:19:46 -0500

    Structure Ready

--- moving_app/app/(pages)/appPage/pages.tsx
+++ moving_app/app/(pages)/appPage/pages.tsx
@@ -1,5 +1,13 @@
-export default function App(){
-    return (
-        <p> Hello World</p>
+import Navbar from "/components/Navbar";
+import ItemList from "/components/ItemList";
+import Footer from "/components/Footer";
+
+export default function AppPage(){
+    return ( 
+        <>
+            <Navbar />
+            <ItemList />
+            <Footer />
+        </>
     );
 }
--- moving_app/app/(pages)/loginPage/pages.tsx
+++ moving_app/app/(pages)/loginPage/pages.tsx
@@ -1,5 +1,5 @@
 export default function Login(){
     return(
-      "Login Page"  
+      <p>Login Page</p> 
     );
 }
