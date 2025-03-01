8ab6b1123b8bfb1647f88ed85242b48ca2939f53
Anna Kester <akester@middlebury.edu>
2025-03-01 13:07:32 -0500

    search functionality

--- moving_app/components/itemList/ItemList.js
+++ moving_app/components/itemList/ItemList.js
@@ -2,8 +2,13 @@ import ItemCard from "../itemCard/ItemCard";
 import listings from "../../lib/item-listings.json";
 
 export default function ItemList({searchTerm}){
+    let listingsTemp = [...listings];
+    if (searchTerm) {
+        listingsTemp = listings.filter((listing) => listing.tags.includes(searchTerm));
+    }
     
-    const items = listings.map((listing) => (
+
+    const items = listingsTemp.map((listing) => (
         <ItemCard
             key={listing.id}
             listing={listing}
