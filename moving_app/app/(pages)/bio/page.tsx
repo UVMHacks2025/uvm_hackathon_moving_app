d0c775154891fb4fc268b4b9b94fab18deaa2d83
noahrizika <noahrizika@gmail.com>
2025-03-01 16:21:11 -0500

    update bio to lukka

--- moving_app/app/(pages)/bio/page.tsx
+++ moving_app/app/(pages)/bio/page.tsx
@@ -6,8 +6,8 @@ import data from "@/lib/seed.json";
 
 export default function Bio() {
   const user = {
-    name: "Noah Rizika",
-    email: "nrizika@middlebury.edu",
+    name: "Lukka Wolff",
+    email: "lawolff@middlebury.edu",
     id: 1738,
     bio: "I am a student at Middlebury College! I love to play soccer and I often enjoy painting in my free time. Unfortunately I am graduating so I need to sell my stuff. I am open to pricing negotiation!",
     savedItems: ["item1", "item2", "item3", "item4"],
